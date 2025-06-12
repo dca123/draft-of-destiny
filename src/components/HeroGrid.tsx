a292937ca68acfe5ff50d5f52098d8d5dc40a073
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-12 16:57:26 +0530

    list

--- src/components/HeroGrid.tsx
+++ src/components/HeroGrid.tsx
@@ -1,14 +1,25 @@
-import { useSetAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 import { selectedHeroAtom } from "./hero-selection-state";
+import { cn } from "@/lib/utils";
 
 export function HeroGrid(props: {
   heroes: Array<{ id: number; shortName: string }>;
 }) {
-  const setSelectedHero = useSetAtom(selectedHeroAtom);
+  const [selectedHero, setSelectedHero] = useAtom(selectedHeroAtom);
   return (
     <div className="grid grid-cols-11 gap-1">
       {props.heroes.map((hero) => (
-        <div key={hero.id} onClick={() => setSelectedHero(hero.shortName)}>
+        <div
+          key={hero.id}
+          onClick={() => setSelectedHero(hero.shortName)}
+          className="relative"
+        >
+          <div
+            className={cn(
+              "absolute inset-0",
+              selectedHero === hero.shortName && "border-3 border-blue-300",
+            )}
+          />
           <img
             src={`https://courier.spectral.gg/images/dota/portraits_vert/${hero.shortName}`}
             className="rounded"
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,15 @@
 import { drizzle } from "drizzle-orm/libsql";
 import path from "path";
 
+import { readdir } from "fs/promises";
+
+const folderPath = process.cwd();
+const files = await readdir(folderPath);
+
+for (const file of files) {
+  console.log(file);
+}
+
 const dbLocation = path.join(process.cwd(), "dota.db");
 export const dotaDb = drizzle({
   connection: {
