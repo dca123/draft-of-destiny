871744ddb1a5cd333b6e17e74a690c8f98eff517
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-12 16:38:05 +0530

    target

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart()],
+  plugins: [tsConfigPaths(), tanstackStart({ target: "vercel" })],
 });
