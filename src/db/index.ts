be288327252019b6066a1526f1a1cf1a73a46899
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-12 17:08:09 +0530

    use turso

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,22 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
 
-const fs = require("fs");
-const path = require("path");
-
-const folderPath = process.cwd();
-
-fs.readdir(folderPath, (err: unknown, files: Array<unknown>) => {
-  if (err) {
-    return console.error("Unable to scan directory:", err);
-  }
-  files.forEach((file) => {
-    console.log(file);
-  });
-});
-
-const dbLocation = path.join(process.cwd(), "dota.db");
 export const dotaDb = drizzle({
   connection: {
-    url: "file:" + dbLocation,
+    url: process.env.TURSO_URL!,
+    authToken: process.env.TURSO_API_KEY!,
   },
 });
