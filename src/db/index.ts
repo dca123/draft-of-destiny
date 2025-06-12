012012c0abfa9f05d39ec4b1cbe4054b8f6d28b0
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-12 16:44:16 +0530

    change db location

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/libsql";
+import path from "path";
 
+const dbLocation = path.join(process.cwd(), "dota.db");
 export const dotaDb = drizzle({
   connection: {
-    url: "file:./dota.db",
+    url: "file:" + dbLocation,
   },
 });
