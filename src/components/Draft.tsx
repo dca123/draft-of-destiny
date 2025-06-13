e842ff975aa5eaa0218458ebb780a9b8b3ed9512
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-13 16:19:38 +0530

    url

--- src/components/Draft.tsx
+++ src/components/Draft.tsx
@@ -19,7 +19,8 @@ export function Draft() {
   });
   const ws = usePartySocket({
     // usePartySocket takes the same arguments as PartySocket.
-    host: process.env.VITE_PARTYKIT_URL,
+    //@ts-expect-error
+    host: import.meta.env.VITE_PARTYKIT_URL!,
     room: "my-room",
 
     // in addition, you can provide socket lifecycle event handlers
